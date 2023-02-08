1a1da43c7a774246f80c9d3b3c9626fbf31d48da
Charlie Oliver <2531385o@student.gla.ac.uk>
2023-02-08 17:27:22 +0000

    Added list of Categories on index page

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+
 def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # # that will be passed to the template engine.
 
-    context_dict = {'boldmessage': 'Crunch, creamy, cookie, candy, cupcake'}
+    category_list = Category.objects.order_by('-likes')[:5]
 
+    context_dict = {}
+    context_dict['boldmessage'] =  'Crunch, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    # return render response and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 
