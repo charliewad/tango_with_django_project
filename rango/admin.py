f993140644404ba3d43ab9d9636504e9bd280f66
Charlie Oliver <2531385o@student.gla.ac.uk>
2023-02-08 16:58:02 +0000

    Overrode admin base site html

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from rango.models import Category, Page, Question
+from rango.models import Category, Page, Question, Choice
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Page)
-admin.site.register(Question)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
